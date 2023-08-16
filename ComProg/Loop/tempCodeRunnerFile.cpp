4453f359325335d84cf962dce14dc54c45344c2e
Itttikorn <ittikornsd@gmail.com>
2023-08-16 09:54:03 +0700

    deleted:    ComProg/Loop/tempCodeRunnerFile.cpp

--- ComProg/Loop/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-                count=count;
-            }else if((i+textlength < searchlength)&&((search[i+textlength]>='A' &&(search[i+textlength]>='Z') || (search[i+textlength]>='a') && (search[i+textlength]>='z')))){
-                count=count;
-            }else{
-                count++;
