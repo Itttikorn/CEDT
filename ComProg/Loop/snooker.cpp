8b85c949c9f5b3558a93a5d759f25a984c51c722
Itttikorn <ittikornsd@gmail.com>
2023-08-18 23:44:15 +0700

    modified:   ComProg/Loop/snooker.cpp

--- ComProg/Loop/snooker.cpp
+++ ComProg/Loop/snooker.cpp
@@ -1,61 +1,90 @@
 #include<iostream>
-#include<string>
 using namespace std;
-int main (){
-    int n;
-    string s1,t = "YGNBPK";
-    cin >> n;
-    for(int i = 0;i<n+1;i++){
-        int sum = 0;
-        string s = "";
-        getline(cin, s1);
-        if(s1 == ""){
-            continue;
-        }
-        for(int j=0;j<s1.length();j+=2){
-            s+=s1[j];
-            //cout << "s= "<<s<<endl;
-        }
-        // if(s[0] != 'R'){
-        //     cout << "WRONG_INPUT" <<endl;
-        //     continue;
-        // }
-        //cout << s<<endl;
-        if (s[0] != 'R'){
-            cout << "WRONG_INPUT"<<endl;
-            continue;
-        }
-        for(int j = 0;j<s.length();j++){
-            //cout << "j = "<<j <<"  s[j] = " << s[j]<<endl;
-            if(j % 2 == 1 && s[j] == 'R'){
-                cout << "WRONG_INPUT"<<endl;
-                break;
-            }
-            else if(j%2 == 0 && s[j] != 'R'){
-                bool check = 1;
-                if(s.substr(j,s.length() - j) != t.substr(0,s.length()-j)){
-                    cout << "WRONG_INPUT"<<endl;
-                    j = s.length();
+
+int main(){
+    int linecount=0;
+    string maxline;
+    getline(cin,maxline);
+    int maxlinenum = stoi(maxline);
+    while(linecount<maxlinenum){
+        linecount++;
+        string input;
+        bool wrong=false,final=false,ended=false;
+        int score=0;
+        getline(cin,input);
+        if(input[0]!='R'){
+            wrong=true;
+        }else{
+            for(int i=2;i<input.length();i+=2){
+                if(ended==true){
+                    wrong=true;
                 }
-                else{
-                    sum += (2+(s.length()-j+1))*(s.length()-j)/2;
-                    cout << sum << endl;
-                    j = s.length();
+   
+                if(i%4==0 && i>=2 && input[i-2]=='R'){
+                    wrong=true;
+                    //cout << "case01" << endl;
                 }
-            }
-            else if(j%2 == 0){
-                sum+=1;
-            }
-            else if(j%2 == 1){
-                for(int k=0;k<6;k++){
-                    if(s[j] == t[k]){
-                        sum += k+2;
+                // if(input[i]=='R' && input[i+2]=='R'){
+                //     wrong=true;
+                //     //cout << "case02" << endl;
+                // }
+                if(input[i]=='R' && input[i-2]=='R'){
+                    wrong=true;
+                }
+                if(final == false && i%4==0 && input[i]!='R' && input[i]!='Y'){
+                    wrong=true;
+                    //cout << "case03" << endl;
+                }    
+                if(i==input.length()-1){
+                    final=true;
+                    //cout << "final" << endl;
+                }
+                if(input[i]=='Y' && input[i-2]!='R'){
+                    final=true;
+                    //cout << "final" << endl;
+                    if(input[i]=='Y' && input[i+2]!='G' && i!=input.length()-1){
+                        wrong=true;
+                        //cout << "case1" << endl;
+                    }else if(input.length()>=i+4 && input[i]=='Y' && input[i+2]=='G' && input[i+4]!='N'){
+                        wrong=true;
+                        //cout << "case2" << endl;
+                    }else if(input.length()>=i+6 && input[i]=='Y' && input[i+2]=='G' && input[i+4]=='N' && input[i+6]!='B'){
+                        wrong=true;
+                        //cout << "case3" << endl;
+                    }else if(input.length()>=i+8 && input[i]=='Y' && input[i+2]=='G' && input[i+4]=='N' && input[i+6]=='B' && input[i+8]!='P'){
+                        wrong=true;
+                        //cout << "case4" << endl;
+                    }else if(input.length()>=i+10 && input[i]=='Y' && input[i+2]=='G' && input[i+4]=='N' && input[i+6]=='B' && input[i+8]=='P' && input[i+10]!='K'){
+                        wrong=true;
+                        //cout << "case5" << endl;
                     }
                 }
-            }
-            if(j == s.length()-1){
-                cout << sum<<endl;
+                if(input[i]=='K' && input[i-2]=='P' && input[i-4]=='B' && input[i-6]=='N' && input[i-8]=='G' && input[i-10]=='Y'){
+                    ended=true;
+                    //cout << "end" << endl;
+                }
+                if(input[i]=='R'){
+                    score+=1;
+                }else if(input[i]=='Y'){
+                    score+=2;
+                }else if(input[i]=='G'){
+                    score+=3;
+                }else if(input[i]=='N'){
+                    score+=4;
+                }else if(input[i]=='B'){
+                    score+=5;
+                }else if(input[i]=='P'){
+                    score+=6;
+                }else if(input[i]=='K'){
+                    score+=7;
+                }
             }
         }
+        if(wrong==true){
+            cout << "WRONG_INPUT" << endl;
+        }else{
+            cout << score+1 << endl;
+        }
+
     }
 }
