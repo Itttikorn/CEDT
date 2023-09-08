30f98de6caa0303abe39ed2145bd3fe9d8b3e5e8
Itttikorn <ittikornsd@gmail.com>
2023-09-08 11:41:34 +0700

    modified:   ComProg/Vector/upgrade.cpp 	modified:   ComProg/Vector/upgrade.exe

--- ComProg/Vector/upgrade.cpp
+++ ComProg/Vector/upgrade.cpp
@@ -2,24 +2,34 @@
 using namespace std;
 
 string update(string in){
-    if(in == "A"){
-        return "A";
-    }else if(in=="B+"){
-        return "A";
-    }else if(in=="B"){
-        return "B+";
-    }else if(in=="C+"){
-        return "B";
-    }else if(in=="C"){
-        return "C+";
-    }else if(in=="D+"){
-        return "C";
-    }else if(in=="D"){
-        return "D+";
-    }else if(in=="F"){
-        return "D";
-    }else{
-        return "F";
+    // if(in == "A"){
+    //     return "A";
+    // }else if(in=="B+"){
+    //     return "A";
+    // }else if(in=="B"){
+    //     return "B+";
+    // }else if(in=="C+"){
+    //     return "B";
+    // }else if(in=="C"){
+    //     return "C+";
+    // }else if(in=="D+"){
+    //     return "C";
+    // }else if(in=="D"){
+    //     return "D+";
+    // }else if(in=="F"){
+    //     return "D";
+    // }else{
+    //     return "F";
+    // }
+    vector<string> grades = {"A","B+","B","C+","C","D+","D","F"};
+    for(int i=0;i<grades.size();i++){
+        if(grades[i]==in){
+            if(in=="A"){
+                return "A";
+            }else{
+                return grades[i-1];
+            }
+        }
     }
 }
 int main(){
