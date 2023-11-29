7f1c97ec81f031286f2aca1510d1ec011cd9e7d5
Itttikorn <ittikornsd@gmail.com>
2023-11-29 15:17:04 +0700

    modified:   DataStructureAndAlgorithms/Algorithms/shortestpathwithnegativeedge.cpp

--- DataStructureAndAlgorithms/Algorithms/shortestpathwithnegativeedge.cpp
+++ DataStructureAndAlgorithms/Algorithms/shortestpathwithnegativeedge.cpp
@@ -5,36 +5,29 @@ int main(){
     int n,e,s;
     cin >> n >> e >> s;
     vector<int> dist(n,100000);
-    vector<vector<pair<int,int>>> edges(n);
+    vector<pair<pair<int,int>,int>> edges;
     for(int i=0;i<e;i++){
         int a,b,c;
         cin >> a >> b >> c;
-        edges[a].push_back({b,c});
+        edges.push_back({{a,b},c});
         //edges.push_back({{b,a},c});
     }
     dist[s] = 0;
     //sort(edges.begin(),edges.end());
     for(int i=0;i<n-1;i++){
-        for(int a=0;a<n;a++){
-            for(auto &v : edges[a]){
-                int b = v.first;
-                int w = v.second;
-                if(dist[b] > dist[a] + w){
-                    dist[b] = dist[a] + w;
-                }
-            }
-        }
-        
-    }
-    for(int a=0;a<n;a++){
-        for(auto &v : edges[a]){
-            int b = v.first;
+        for(auto &v : edges){
+            int a = v.first.first;
+            int b = v.first.second;
             int w = v.second;
             if(dist[b] > dist[a] + w){
                 dist[b] = dist[a] + w;
-                cout << -1;
-                return 0;
             }
+        }
+    }
+    for(auto &e : edges){
+        if(dist[e.first.second]>dist[e.first.first]+e.second){
+            cout << -1;
+            return 0;
         }
     }
     for(int i=0;i<n;i++){
