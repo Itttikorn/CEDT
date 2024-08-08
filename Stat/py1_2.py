61db8ffe0f9f7b0698dbfbd447e0eb675cac7833
Itttikorn <ittikornsd@gmail.com>
2024-08-08 13:53:44 +0700

    make it a func

--- Stat/py1_2.py
+++ Stat/py1_2.py
@@ -1,13 +1,15 @@
 import numpy as np
-a = np.matmul(np.transpose(np.array([[1,3,-2],
+def calculate():
+    a = np.matmul(np.transpose(np.array([[1,3,-2],
                             [4,5,6],
                             [0,3,4]])),np.array([[1],
                                                 [2],
                                                 [3]]))
 
-b = np.matmul(np.array([[1,3],
-            [4,5],
-            [0,3]]), np.array([[1,3,-2],
-                            [4,5,-1]])) 
+    b = np.matmul(np.array([[1,3],
+                [4,5],
+                [0,3]]), np.array([[1,3,-2],
+                                [4,5,-1]])) 
 
-print(np.sum(a)+np.sum(np.diagonal(b)))
+    print(np.sum(a)+np.sum(np.diagonal(b)))
+calculate()
